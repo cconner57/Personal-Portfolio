d5aa00f2b8f7e6927ce8fe14e82968e2316090b9
Chris <chris.conner57@gmail.com>
2020-01-30 11:04:06 -0800

    Updated Nav

--- portfolio-site/src/components/Navbar.js
+++ portfolio-site/src/components/Navbar.js
@@ -5,48 +5,19 @@ import { HashLink as Link } from 'react-router-hash-link';
 function Navbar() {
 	return (
 		<div className="navbar-fixed">
-			<nav className="nav-extended green valign-wrapper">
-				<Router>
-					<div className="nav-wrapper container">
-						<div className="container ">
-							<span className="brand-logo">Chris</span>
-						</div>
-						<ul id="nav-mobile" className="right hide-on-med-and-down">
-							<li>
-								<Link smooth to="/#about">
-									About
-								</Link>
-							</li>
-							<li>
-								<Link smooth to="/#skills">
-									Skills
-								</Link>
-							</li>
-							<li>
-								<Link smooth to="/#projects">
-									Projects
-								</Link>
-							</li>
-							<li>
-								<Link smooth to="/#contact">
-									Contact
-								</Link>
-							</li>
-						</ul>
-					</div>
-				</Router>
-				<div className="nav-content hide-on-large-only container">
-					<ul className="tabs tabs-transparent">
-						<li className="tab">
+			<nav className="nav-extended light-blue accent-4">
+				<div className="nav-content container">
+					<ul className="tabs tabs-transparent row">
+						<li className="tab col s3">
 							<a href="#about">About</a>
 						</li>
-						<li className="tab">
+						<li className="tab col s3">
 							<a href="#skills">Skills</a>
 						</li>
-						<li className="tab">
+						<li className="tab col s3">
 							<a href="#projects">Projects</a>
 						</li>
-						<li className="tab">
+						<li className="tab col s3">
 							<a href="#contact">Contact</a>
 						</li>
 					</ul>
